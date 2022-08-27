ffa6c06567650a7737cfafbffce7b89803c1e243
josephakeni <josephakeni@gmail.com>
2022-08-27 10:49:46 +0100

    Route traffic and filtering added

--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,14 @@ const router = express.Router();
 require('dotenv').config();
 let mysql = require('mysql2');
 
-
+// /admin/add-pproduct => GET
 router.get('/add-product', function(req, res){
     // res.send("");
-    res.send('<h1>Hello from add-product page!!! </h1><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.send('<h1>Hello from add-product page!!! </h1><form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
  });
 
- router.post('/product', (req, res)=> {
+ // /admin/add-pproduct => POST
+ router.post('/add-product', (req, res)=> {
     console.log(req.body);
     res.redirect('/');
  });
