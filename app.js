27a20a2f4f622bbc6a29c24398f9dd14b4a14b28
josephakeni <josephakeni@gmail.com>
2022-09-03 02:51:38 +0000

    Cart Functionality  Added

--- app.js
+++ app.js
@@ -8,15 +8,30 @@ const PORT = process.env.APP_PORT;
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { constants } = require('buffer');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+  .then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => console.log(err));
+});
 
 // Route traffic and filtering
 app.use('/admin', adminRoutes);
@@ -25,8 +40,30 @@ app.use(shopRoutes);
 // Add 404 Error Page
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
-  console.log(result);
+///  Databbse  Relations
+Product.belongsTo(User, { constants: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem })
+
+sequelize
+//.sync({ force: true })
+.sync()
+.then(result => {
+  return User.findByPk(1);
+  // console.log(result);
+}).then(user => {
+  if (!user) {
+    return User.create({ name: 'Joseph', email: 'josephakeni@gmail.com'});
+  }
+  return user;
+})
+.then(user => {
+  // console.log(user);
+  return user.createCart();
+}).then(cart => {
   app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`)
   });
